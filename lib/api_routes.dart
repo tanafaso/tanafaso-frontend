3308d004a406153d6dc961139b30e615b8dfee4a
Omar Yasser <oyaraouf@gmail.com>
2020-05-10 16:48:22 +0200

    Allow user to login with facebook
    This CL extends the login screen functionality to:
    - Allow the user to login with facebook.
    - Send facebook access token to the API.
    - Retrieve user API JWT.

---
+++ lib/api_routes.dart
@@ -0,0 +1,4 @@
+class ApiRoutes {
+  static final String BASE_URL = 'localhost:8080';
+  static final String LOGIN_WITH_FACEBOOK = '/login/facebook';
+}
--- lib/authentication.dart
+++ lib/authentication.dart
@@ -1,24 +1,57 @@
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:io';
+
+import 'package:azkar/api_routes.dart';
+import 'package:azkar/payload/authentication/requests/facebook_authentication_request.dart';
+import 'package:azkar/payload/response_error.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+
+import 'payload/authentication/responses/facebook_authentication_response.dart';
 
 class Authentication {
   static final _facebookLogin = FacebookLogin();
+
+  // The context that from which the authentication is fired.
   static var _facebookAccessToken;
 
-  static Future<void> loginWithFacebook() async {
+  static Future<FacebookAuthenticationResponse> loginWithFacebook() async {
     _facebookLogin.loginBehavior = FacebookLoginBehavior.webViewOnly;
 
-    final result = await _facebookLogin.logIn(['email']);
+    final facebookGraphApiResponse = await _facebookLogin.logIn(['email']);
+    FacebookAuthenticationResponse facebookAuthenticationResponse;
 
-    switch (result.status) {
+    switch (facebookGraphApiResponse.status) {
       case FacebookLoginStatus.loggedIn:
-        _facebookAccessToken = result.accessToken.token;
-        // TODO: send to the backend
-        break;
+        _facebookAccessToken = facebookGraphApiResponse.accessToken.token;
+
+        final http.Response apiResponse = await http.put(
+            Uri.http(ApiRoutes.BASE_URL, ApiRoutes.LOGIN_WITH_FACEBOOK),
+            headers: <String, String>{
+              'Content-Type': 'application/json; charset=UTF-8',
+            },
+            body: jsonEncode(new FacebookAuthenticationRequest(
+              facebookUserId: facebookGraphApiResponse.accessToken.userId,
+              token: facebookGraphApiResponse.accessToken.token,
+            ).toJson()));
+
+        facebookAuthenticationResponse =
+            FacebookAuthenticationResponse.fromJson(
+                jsonDecode(apiResponse.body));
+        if (facebookAuthenticationResponse.error.error_message == null) {
+          final jwtToken = apiResponse.headers[HttpHeaders.authorizationHeader];
+          final _storage = FlutterSecureStorage();
+          await _storage.write(key: 'jwtToken', value: jwtToken);
+        }
+        return new Future.value(facebookAuthenticationResponse);
       case FacebookLoginStatus.cancelledByUser:
+        facebookAuthenticationResponse.error = new Error("Cancelled by user.");
+        return new Future.value(facebookAuthenticationResponse);
       case FacebookLoginStatus.error:
-        break;
+        facebookAuthenticationResponse.error =
+            new Error("Facebook login error.");
+        return new Future.value(facebookAuthenticationResponse);
     }
   }
 
