818cc878cccc9c0bccb47a8cbee952392e494093
Omar Yasser <oyaraouf@gmail.com>
2020-06-01 16:47:34 +0200

    Add first step in email registration
    The first step is accepting user's data and sending them to the API and
    either display the error message returned from the API or do nothing
     (Extending the UI to accept a PIN is the second step).

--- lib/payload/response_base.dart
+++ lib/payload/response_base.dart
@@ -12,6 +12,14 @@ abstract class ResponseBase<T> {
     _error = value;
   }
 
+  setError(Map<String, dynamic> json) {
+    _error = new Error((json['error'] ?? const {}) ['message']);
+  }
+
+  bool hasError() {
+    return _error?.error_message?.isEmpty ?? false;
+  }
+
   Error get error => _error;
 
   T get data => _data;
