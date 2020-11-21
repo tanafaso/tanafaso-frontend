e80ba0b531baf623111346fc56a7dc6b347e08b3
Omar Yasser <oyaraouf@gmail.com>
2020-11-21 13:04:08 +0100

    Save

--- lib/views/entities/friends/friend_request_widget.dart
+++ lib/views/entities/friends/friend_request_widget.dart
@@ -1,16 +1,46 @@
 import 'package:azkar/models/friend.dart';
 import 'package:flutter/material.dart';
 
-class FriendRequestWidget extends StatelessWidget {
+class FriendRequestWidget extends StatefulWidget {
   final Friend friend;
 
   FriendRequestWidget({@required this.friend});
 
   @override
+  _FriendRequestWidgetState createState() => _FriendRequestWidgetState();
+}
+
+class _FriendRequestWidgetState extends State<FriendRequestWidget> {
+  @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: [Text('Username: ${friend.username}')],
+      child: Row(
+        children: [
+          Flexible(
+            flex: 10,
+            fit: FlexFit.tight,
+            child: Column(
+              children: [Text('Username: ${widget.friend.username}')],
+            ),
+          ),
+          Flexible(
+            flex: 10,
+            fit: FlexFit.tight,
+            child: Row(
+              children: [
+                RaisedButton(
+                  child: Text('Accept'),
+                  color: Colors.green.shade400,
+                  onPressed: () => {},
+                ),
+                OutlineButton(
+                  child: (Text('Ignore')),
+                  onPressed: () => {},
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
