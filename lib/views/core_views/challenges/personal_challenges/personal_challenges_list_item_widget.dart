c5caf980e26be1e74b6c47e02d39479dbef24560
Omar Yasser <oyaraouf@gmail.com>
2021-04-14 18:43:31 +0200

    Add leftover changes

--- lib/views/core_views/challenges/personal_challenges/personal_challenges_list_item_widget.dart
+++ lib/views/core_views/challenges/personal_challenges/personal_challenges_list_item_widget.dart
@@ -1,7 +1,7 @@
 import 'package:azkar/main.dart';
 import 'package:azkar/models/challenge.dart';
-import 'package:azkar/views/core_views/challenges/group_challenges/group_challenge_list_item_widget.dart';
 import 'package:azkar/views/core_views/challenges/do_challenge/do_challenge_screen.dart';
+import 'package:azkar/views/core_views/challenges/group_challenges/group_challenge_list_item_widget.dart';
 import 'package:flutter/material.dart';
 
 class PersonalChallengesListItemWidget extends StatelessWidget {
