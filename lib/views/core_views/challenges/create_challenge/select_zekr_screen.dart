51bf306041e7ccd7a60301fc044f8a1977d8f349
Omar Yasser <oyaraouf@gmail.com>
2021-04-14 11:33:44 +0200

    Add progress bar (#82)

--- lib/views/core_views/challenges/create_challenge/select_zekr_screen.dart
+++ lib/views/core_views/challenges/create_challenge/select_zekr_screen.dart
@@ -1,13 +1,7 @@
-import 'dart:convert';
-
 import 'package:azkar/main.dart';
 import 'package:azkar/models/zekr.dart';
-import 'package:azkar/net/api_caller.dart';
-import 'package:azkar/net/endpoints.dart';
-import 'package:azkar/net/payload/challenges/responses/get_azkar_response.dart';
 import 'package:azkar/views/core_views/challenges/create_challenge/zekr_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 class SelectZekrScreen extends StatefulWidget {
   final List<Zekr> azkar;
@@ -35,12 +29,12 @@ class _SelectZekrScreenState extends State<SelectZekrScreen> {
   }
 
   void filterBy(String sequence) {
-   filteredAzkar = [];
-   for (Zekr zekr in widget.azkar) {
+    filteredAzkar = [];
+    for (Zekr zekr in widget.azkar) {
       if (zekr.zekr.contains(sequence)) {
         filteredAzkar.add(zekr);
       }
-   }
+    }
   }
 
   @override
@@ -53,31 +47,30 @@ class _SelectZekrScreenState extends State<SelectZekrScreen> {
         padding: const EdgeInsets.all(8.0),
         child: Center(
             child: Column(
-              children: [
-                Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      controller: searchController,
-                      decoration: InputDecoration(
-                        hintText:
-                        AppLocalizations.of(context).searchForAZekr,
-                      ),
-                    ),
+          children: [
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: searchController,
+                  decoration: InputDecoration(
+                    hintText: AppLocalizations.of(context).searchForAZekr,
                   ),
                 ),
-                Flexible(
-                  child: Scrollbar(
-                    child: ListView.builder(
-                      itemCount: filteredAzkar.length,
-                      itemBuilder: (context, index) {
-                        return ZekrWidget(zekr: filteredAzkar[index]);
-                      },
-                    ),
-                  ),
+              ),
+            ),
+            Flexible(
+              child: Scrollbar(
+                child: ListView.builder(
+                  itemCount: filteredAzkar.length,
+                  itemBuilder: (context, index) {
+                    return ZekrWidget(zekr: filteredAzkar[index]);
+                  },
                 ),
-              ],
-            )),
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
