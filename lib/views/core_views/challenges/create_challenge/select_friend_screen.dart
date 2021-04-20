8022e8ec7625165ef278bf6c62afe768f018d75e
Omar Yasser <oyaraouf@gmail.com>
2021-04-20 19:20:42 +0200

    Smoothen loading

--- lib/views/core_views/challenges/create_challenge/select_friend_screen.dart
+++ lib/views/core_views/challenges/create_challenge/select_friend_screen.dart
@@ -1,41 +1,24 @@
-import 'dart:convert';
-
 import 'package:azkar/main.dart';
 import 'package:azkar/models/friend.dart';
-import 'package:azkar/net/api_caller.dart';
-import 'package:azkar/net/endpoints.dart';
-import 'package:azkar/net/payload/users/responses/get_friends_response.dart';
+import 'package:azkar/models/friendship.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 class SelectFriendScreen extends StatelessWidget {
+  final Friendship friendship;
+
+  SelectFriendScreen({@required this.friendship});
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: ApiCaller.get(
-            route: Endpoint(endpointRoute: EndpointRoute.GET_FRIENDS)),
-        builder: (BuildContext context, AsyncSnapshot<http.Response> snapshot) {
-          if (snapshot.hasData) {
-            GetFriendsResponse response = GetFriendsResponse.fromJson(
-                jsonDecode(utf8.decode(snapshot.data.body.codeUnits)));
-            return Scaffold(
-              appBar: AppBar(
-                title: Text(AppLocalizations.of(context).selectAFriend),
-              ),
-              body: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: buildWidgetWithFriends(
-                    context, response?.friendship?.friends ?? []),
-              ),
-            );
-          } else if (snapshot.hasError) {
-            // TODO(omorsi): Handle error
-            return Text(AppLocalizations.of(context).error);
-          } else {
-            // TODO(omorsi): Show loader
-            return Text(AppLocalizations.of(context).loading);
-          }
-        });
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context).selectAFriend),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: buildWidgetWithFriends(context, friendship?.friends ?? []),
+      ),
+    );
   }
 
   Widget buildWidgetWithFriends(BuildContext context, List<Friend> friends) {
