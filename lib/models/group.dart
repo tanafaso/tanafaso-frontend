7f2b2553f3df4a0c4dfcea37d13b8e837a50d012
Omar Yasser <oyaraouf@gmail.com>
2021-04-27 23:02:21 +0200

    Cache groups info (#143)

--- lib/models/group.dart
+++ lib/models/group.dart
@@ -39,3 +39,48 @@ class Group {
         "binary": binary,
       };
 }
+
+class CachedGroupInfo {
+  CachedGroupInfo({
+    this.id,
+    this.name,
+    this.adminId,
+    this.usersIds = const [],
+    // this.challengesIds = const [],
+    this.binary,
+  });
+
+  String id;
+  String name;
+  String adminId;
+  List<String> usersIds;
+  // List<String> challengesIds;
+  bool binary;
+
+  factory CachedGroupInfo.fromGroup(Group group) => CachedGroupInfo(
+        id: group.id,
+        name: group.name,
+        adminId: group.adminId,
+        usersIds: group.usersIds,
+        binary: group.binary,
+      );
+
+  factory CachedGroupInfo.fromJson(Map<String, dynamic> json) =>
+      CachedGroupInfo(
+        id: json["id"],
+        name: json["name"],
+        adminId: json["adminId"],
+        usersIds: List<String>.from(json["usersIds"].map((x) => x)),
+        // challengesIds: List<String>.from(json["challengesIds"].map((x) => x)),
+        binary: json["binary"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "adminId": adminId,
+        "usersIds": List<dynamic>.from(usersIds.map((x) => x)),
+        // "challengesIds": List<dynamic>.from(challengesIds.map((x) => x)),
+        "binary": binary,
+      };
+}
---
+++ lib/net/cache_manager.dart
@@ -0,0 +1,15 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CacheManager {
+  // This design assumes that we will at most need to hash 1000 different
+  // categories of information.
+  static const String CACHE_KEY_CURRENT_USER_ID = "000";
+  static const String CACHE_KEY_USER_FULL_NAME_PREFIX = "001";
+  static const String CACHE_KEY_GROUP_ID_PREFIX = "002";
+
+  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  Future<SharedPreferences> getPrefs() async {
+    return await _prefs;
+  }
+}
