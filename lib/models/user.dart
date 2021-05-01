28cc65bdddbac60d87081829eb8747e41707c8db
Omar Yasser <oyaraouf@gmail.com>
2021-05-01 11:15:32 +0200

    Add finished personal challenges to score (#157)

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,4 @@
+import 'package:azkar/models/challenge.dart';
 import 'package:azkar/models/user_group.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -8,6 +9,7 @@ class User {
   String firstName;
   String lastName;
   List<UserGroup> userGroups;
+  List<Challenge> personalChallenges;
 
   User({
     @required this.email,
@@ -16,12 +18,16 @@ class User {
     @required this.firstName,
     @required this.lastName,
     @required this.userGroups,
+    @required this.personalChallenges,
   });
 
   int getFinishedChallengesCount() {
     int totalScore = 0;
     for (UserGroup userGroup in userGroups) {
       totalScore += userGroup.totalScore;
+    }
+    for (Challenge challenge in personalChallenges) {
+      totalScore += challenge.done() ? 1 : 0;
     }
     return totalScore;
   }
