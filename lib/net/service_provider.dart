6e78b900fece11d6ee9f3163252a71eb622575c0
Omar Yasser <oyaraouf@gmail.com>
2021-05-14 19:49:48 +0200

    Support Azkar categories

--- lib/net/service_provider.dart
+++ lib/net/service_provider.dart
@@ -1,9 +1,10 @@
 import 'package:azkar/net/authentication_service.dart';
+import 'package:azkar/net/cache_manager.dart';
+import 'package:azkar/net/categories_service.dart';
 import 'package:azkar/net/challenges_service.dart';
 import 'package:azkar/net/groups_service.dart';
 import 'package:azkar/net/secure_storage_service.dart';
 import 'package:azkar/net/users_service.dart';
-import 'package:azkar/net/cache_manager.dart';
 import 'package:http/http.dart' as http;
 
 /// Holds instances of the application services. This should be the main access
@@ -16,6 +17,7 @@ class ServiceProvider {
   static UsersService usersService = new UsersService();
   static GroupsService groupsService = new GroupsService();
   static SecureStorageService secureStorageService = new SecureStorageService();
+  static CategoriesService azkarService = new CategoriesService();
   static CacheManager cacheManager = new CacheManager();
   static http.Client httpClient = http.Client();
 }
