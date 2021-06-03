a758f72ed46521c8ee9a8268fbe2ff57aeb8a2f1
Omar Yasser <oyaraouf@gmail.com>
2021-06-03 12:13:13 +0200

    Revert testing code

--- lib/net/authentication_service.dart
+++ lib/net/authentication_service.dart
@@ -56,7 +56,7 @@ class AuthenticationService {
   Future<void> _loginWithFacebookAccessToken(
       FacebookAccessToken facebookAccessToken) async {
     final http.Response apiResponse = await http.put(
-        Uri.http(
+        Uri.https(
             ApiRoutesUtil.apiRouteToString(
                 Endpoint(endpointRoute: EndpointRoute.BASE_URL)),
             ApiRoutesUtil.apiRouteToString(
@@ -131,7 +131,7 @@ class AuthenticationService {
         await ServiceProvider.secureStorageService.getFacebookToken();
     String facebookUserId =
         await ServiceProvider.secureStorageService.getFacebookUserId();
-    http.Response response = await http.get(Uri.http("graph.facebook.com",
+    http.Response response = await http.get(Uri.https("graph.facebook.com",
         "v9.0/$facebookUserId/friends/", {'access_token': facebookToken}));
     if (response.statusCode == FACEBOOK_INVALID_OAUTH_TOKEN_ERROR_CODE) {
       throw ApiException.withDefaultError();
@@ -149,7 +149,7 @@ class AuthenticationService {
     http.Response apiResponse;
     try {
       apiResponse = await http.put(
-        Uri.http(
+        Uri.https(
             ApiRoutesUtil.apiRouteToString(
                 Endpoint(endpointRoute: EndpointRoute.BASE_URL)),
             ApiRoutesUtil.apiRouteToString(
@@ -174,7 +174,7 @@ class AuthenticationService {
     http.Response apiResponse;
     try {
       apiResponse = await http.put(
-        Uri.http(
+        Uri.https(
             ApiRoutesUtil.apiRouteToString(
                 Endpoint(endpointRoute: EndpointRoute.BASE_URL)),
             ApiRoutesUtil.apiRouteToString(
@@ -220,7 +220,7 @@ class AuthenticationService {
     http.Response apiResponse;
     try {
       apiResponse = await http.put(
-        Uri.http(
+        Uri.https(
             ApiRoutesUtil.apiRouteToString(
                 Endpoint(endpointRoute: EndpointRoute.BASE_URL)),
             ApiRoutesUtil.apiRouteToString(
