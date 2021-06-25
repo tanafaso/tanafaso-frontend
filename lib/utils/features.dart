0352268bfacb37d504219bc9897fab1804a511bc
Omar Yasser <oyaraouf@gmail.com>
2021-06-25 07:38:17 +0200

    Revert unneeded changes

--- lib/utils/features.dart
+++ lib/utils/features.dart
@@ -1,8 +1,8 @@
 // Note: Changing any string will lead to showing it even for users who have
 // already saw it.
 class Features {
-  static const String CLONE_AND_DELETE = "clone-and-delete2";
+  static const String CLONE_AND_DELETE = "clone-and-delete";
   static const String CLICK_ZEKR_AFTER_FINISH = "click-zekr-after-finish";
   static const String SHARE_USERNAME = "share-username";
-  static const String SABEQ_INTRODUCTION = "sabeq-introduction5";
+  static const String SABEQ_INTRODUCTION = "sabeq-introduction";
 }
