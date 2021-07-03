648ccc4f5f2445bcc443540c7caa5820dee998c5
Omar Yasser <oyaraouf@gmail.com>
2021-07-03 12:04:00 +0200

    Format

--- lib/net/endpoints.dart
+++ lib/net/endpoints.dart
@@ -54,13 +54,14 @@ class ApiRoutesUtil {
     switch (route.endpointRoute) {
       case EndpointRoute.BASE_URL:
         if (Platform.isAndroid) {
-          return '10.0.2.2:8080';
-          // return 'www.tanafaso.com';
+          // Use the following for testing locally.
+          // return '10.0.2.2:8080';
+          return 'www.tanafaso.com';
         }
         if (Platform.isIOS) {
-          // return 'localhost:8080';
-          return '192.168.2.102:8080';
-          // return 'www.tanafaso.com';
+          // Use the following for testing locally.
+          // return '192.168.2.102:8080';
+          return 'www.tanafaso.com';
         }
         assert(false);
         break;
