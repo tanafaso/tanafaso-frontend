92e30feb9b6233cfbc1b1145bec418e8e34e92e7
Omar Yasser <oyaraouf@gmail.com>
2021-07-05 08:17:24 +0200

    format (#404)

--- lib/views/core_views/challenges/all_challenges/all_challenges_widget.dart
+++ lib/views/core_views/challenges/all_challenges/all_challenges_widget.dart
@@ -18,63 +18,60 @@ class _AllChallengesWidgetState extends State<AllChallengesWidget> {
   Widget build(BuildContext context) {
     return Container(
       child: FutureBuilder<List<dynamic>>(
-          future: Future.wait([
+        future: Future.wait([
           ServiceProvider.challengesService.getAllChallenges(),
           ServiceProvider.groupsService.getGroups(),
           ServiceProvider.usersService.getFriendsLeaderboard(),
-          ]),
-      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
-        List<Widget> children;
-        if (snapshot.hasData) {
-          return getChallengesListWidget(
-              snapshot.data[0], snapshot.data[1], snapshot.data[2]);
-        } else if (snapshot.hasError) {
-          children = <Widget>[
-            Icon(
-              Icons.error_outline,
-              color: Colors.red,
-              size: 60,
+        ]),
+        builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+          List<Widget> children;
+          if (snapshot.hasData) {
+            return getChallengesListWidget(
+                snapshot.data[0], snapshot.data[1], snapshot.data[2]);
+          } else if (snapshot.hasError) {
+            children = <Widget>[
+              Icon(
+                Icons.error_outline,
+                color: Colors.red,
+                size: 60,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: SnapshotUtils.getErrorWidget(context, snapshot),
+              )
+            ];
+          } else {
+            children = <Widget>[
+              SizedBox(
+                child: CircularProgressIndicator(),
+                width: 60,
+                height: 60,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                    '${AppLocalizations.of(context).loadingTheChallenges}...'),
+              )
+            ];
+          }
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: children,
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: SnapshotUtils.getErrorWidget(context, snapshot),
-            )
-          ];
-        } else {
-          children = <Widget>[
-            SizedBox(
-              child: CircularProgressIndicator(),
-              width: 60,
-              height: 60,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 16),
-              child: Text(
-                  '${AppLocalizations
-                      .of(context)
-                      .loadingTheChallenges}...'),
-            )
-          ];
-        }
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: children,
-          ),
-        );
-      },
-    ),);
+          );
+        },
+      ),
+    );
   }
 
-  Widget getChallengesListWidget(List<Challenge> challenges,
-      List<Group> groups, List<FriendshipScores> friendshipScores) {
+  Widget getChallengesListWidget(List<Challenge> challenges, List<Group> groups,
+      List<FriendshipScores> friendshipScores) {
     if (challenges == null || challenges.isEmpty) {
       return Center(
         child: Text(
-          AppLocalizations
-              .of(context)
-              .noChallengesFound,
+          AppLocalizations.of(context).noChallengesFound,
           key: Keys.allChallengesWidgetNoChallengesFoundKey,
         ),
       );
@@ -90,14 +87,10 @@ class _AllChallengesWidgetState extends State<AllChallengesWidget> {
         key: Keys.allChallengesWidgetListKey,
         addAutomaticKeepAlives: true,
         // Cache half screen after and half screen before the current screen.
-        cacheExtent: MediaQuery
-            .of(context)
-            .size
-            .height * 0.5,
-        separatorBuilder: (context, index) =>
-            Padding(
-              padding: EdgeInsets.only(bottom: 4),
-            ),
+        cacheExtent: MediaQuery.of(context).size.height * 0.5,
+        separatorBuilder: (context, index) => Padding(
+          padding: EdgeInsets.only(bottom: 4),
+        ),
         itemCount: challenges.length,
         itemBuilder: (context, index) {
           return GroupChallengeListItemWidget(
