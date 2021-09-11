6249f2f2d88d4e27fbdb48d4830e075674ed67a0
Omar Yasser <oyaraouf@gmail.com>
2021-09-11 08:30:46 +0200

    Add friend feature discovery

--- lib/utils/features.dart
+++ lib/utils/features.dart
@@ -3,6 +3,7 @@
 class Features {
   static const String CLONE_AND_DELETE = "clone-and-delete";
   static const String CLICK_ZEKR_AFTER_FINISH = "click-zekr-after-finish";
+  static const String ADD_FRIEND = "add-friend";
   static const String SHARE_USERNAME = "share-username";
   static const String SABEQ_INTRODUCTION = "sabeq-introduction";
   static const String ADD_CHALLENGE = "ADD_CHALLENGE";
